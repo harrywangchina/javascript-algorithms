1be9e6b9040d2a14441319332273fe693a7addb5
Harry Wang <hardhardcoding@gmail.com>
2019-08-06 11:07:30 +0800

    add License file

--- util.js
+++ util.js
@@ -11,3 +11,39 @@ function test() {
 }
 // var time = time_my_script('test');
 // console.log("spend: " + time);
+merge();
+function merge() {
+    var result = _mergeSortRec([1, 6, 2, 4]);
+    console.log(result);
+}
+function _mergeSortRec(array) {
+    var length = array.length;
+    if (length === 1) {
+        return array;
+    }
+    var mid = Math.floor(length / 2),
+        left = array.slice(0, mid),
+        right = array.slice(mid, length);
+    return _merge(_mergeSortRec(left), _mergeSortRec(right));
+}
+function _merge(left, right) {
+    var result = [],
+        il = 0,
+        ir = 0
+        leftLength = left.length,
+        rightLength = right.length;
+    while(il < leftLength && ir < rightLength) {
+        if (left[il] > right[ir]) {
+            result.push(right[ir++]);
+        } else {
+            result.push(left[il++]);
+        }
+    }
+    while(il < leftLength) {
+        result.push(left[il++]);
+    }
+    while(ir < rightLength) {
+        result.push(right[ir++]);
+    }
+    return result;
+}
